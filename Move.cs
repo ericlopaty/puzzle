a5e327e7254129e5f13c35f397ef49e737944952
Eric Lopaty <elopaty@bioreference.com>
2017-05-30 09:01:56 -0400

    update to vs2015

--- Move.cs
+++ Move.cs
@@ -3,29 +3,29 @@ using System.Text;
 
 namespace puzzle
 {
-	public class Move
-	{
-		int pegFrom,pegOver,pegTo;
+    public class Move
+    {
+        int pegFrom, pegOver, pegTo;
 
-		public Move(int pegFrom,int pegOver,int pegTo)
-		{
-			this.pegFrom=pegFrom;
-			this.pegOver=pegOver;
-			this.pegTo=pegTo;
-		}
-		public bool IsPossible(StringBuilder s)
-		{
-			return (s[pegFrom]=='X' && s[pegOver]=='X' && s[pegTo]=='O');
-		}
-		public void MakeMove(ref StringBuilder s)
-		{
-			s[pegFrom]=s[pegOver]='O';
-			s[pegTo]='X';
-		}
-		public override string ToString()
-		{
-			char[] letters={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'};
-			return (letters[pegFrom])+"/"+(letters[pegOver])+" ";
-		}
-	}
+        public Move(int pegFrom, int pegOver, int pegTo)
+        {
+            this.pegFrom = pegFrom;
+            this.pegOver = pegOver;
+            this.pegTo = pegTo;
+        }
+        public bool IsPossible(StringBuilder s)
+        {
+            return (s[pegFrom] == 'X' && s[pegOver] == 'X' && s[pegTo] == 'O');
+        }
+        public void MakeMove(ref StringBuilder s)
+        {
+            s[pegFrom] = s[pegOver] = 'O';
+            s[pegTo] = 'X';
+        }
+        public override string ToString()
+        {
+            char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O' };
+            return (letters[pegFrom]) + "/" + (letters[pegOver]) + " ";
+        }
+    }
 }
